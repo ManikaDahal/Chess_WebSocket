3c53026ff2666a0ef5bffc2dd6fc0d090715e5d4
Manika Dahal <manikadahal8@gmail.com>
2026-01-18 17:59:24 +0545

    Added Home Page

--- websocket_project/urls.py
+++ websocket_project/urls.py
@@ -3,6 +3,10 @@
 
 from django.urls import path
 
+
+def home(request):
+    return HttpResponse("WebSocket is running successfully ")
 urlpatterns = [
     # No REST API endpoints - all handled by Vercel deployment
+    path('', home),
 ]
