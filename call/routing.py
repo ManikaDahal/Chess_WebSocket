2e130afc3689754eb0788c9bffed8a36223f516e
Manika Dahal <manikadahal8@gmail.com>
2026-01-27 14:15:09 +0545

    Edited routing.py

--- call/routing.py
+++ call/routing.py
@@ -6,6 +6,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/call/(?P<room_name>\w+)/$', consumers.CallConsumer.as_asgi()),
-    re_path(r'ws/chat/(?P<room_id>\w+)/$', ChatConsumer.as_asgi()),
+    re_path(r'ws/call/(?P<room_name>[^/]+)/?$', consumers.CallConsumer.as_asgi()),
+    re_path(r'ws/chat/(?P<room_id>[^/]+)/?$', ChatConsumer.as_asgi()),
 ]
