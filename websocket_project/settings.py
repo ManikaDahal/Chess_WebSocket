84f1ab43e2ccc59970536ead1b0dcb6853514a6c
Manika Dahal <manikadahal8@gmail.com>
2026-02-11 18:35:39 +0545

    Changed debug to true to find error

--- websocket_project/settings.py
+++ websocket_project/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'unsafe-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = True # os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = [
     '.onrender.com',
