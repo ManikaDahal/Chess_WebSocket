fca9c3237eae641b0bb0999e064bf3178f8929a8
Manika Dahal <manikadahal8@gmail.com>
2026-03-20 14:00:06 +0545

    Fixed admin page error

--- websocket_project/settings.py
+++ websocket_project/settings.py
@@ -146,9 +146,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-# Allow deployment to succeed even if some static files are missing from manifest
+# Allow deployment to succeed even if some static files are missing
 WHITENOISE_MANIFEST_STRICT = False
 
 # Media files (User uploads - videos, thumbnails)
